24f8fc6164bd6f4c66062cbd37a0cfecb1323f38
Anish <xevenbiswas@gmail.com>
2024-07-02 21:40:17 +0530

    add shadcn

--- src/app/pw/page.tsx
+++ src/app/pw/page.tsx
@@ -1,15 +1,9 @@
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default async function Protected() {
-  const { isAuthenticated } = getKindeServerSession();
-
-  return (await isAuthenticated()) ? (
+export default async function page() {
+  return (
     <div>
       This page is protected - but you can view it because you are authenticated
-    </div>
-  ) : (
-    <div>
       This page is protected, please <LoginLink>Login</LoginLink> to view it
     </div>
   );
